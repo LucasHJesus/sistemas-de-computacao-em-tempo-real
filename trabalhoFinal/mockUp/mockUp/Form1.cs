ac6f69b4633115de974a3202bbe2ce1353029ff7
LucasJesus <lukaslima641@gmail.com>
2024-04-18 21:46:06 -0300

    mockup

--- trabalhoFinal/mockUp/mockUp/Form1.cs
+++ trabalhoFinal/mockUp/mockUp/Form1.cs
@@ -17,9 +17,22 @@ namespace mockUp
         double longitude;
         string formatoPacoteModulo2;
         byte[] bytesAEnviarModulo2;
+
+        double maxLat;
+        double minLat;
+        double maxLong;
+        double minLong;
+
+        bool flagStop;
+
         public Form1()
         {
             InitializeComponent();
+            maxLat = -18.85;
+            minLat = -18.98;
+            maxLong = -48.2;
+            minLong = -48.37;
+            flagStop = false;
         }
 
         private void rbNormal_CheckedChanged(object sender, EventArgs e)
@@ -35,6 +48,13 @@ namespace mockUp
         private void rbFurtoDeEnergia_CheckedChanged(object sender, EventArgs e)
         {
             modoDeGeracao = 3;
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            if (usocketConexaoUDPModulo2 != null)
+                usocketConexaoUDPModulo2.Close();
+            flagStop = true;
         }
 
         private void btnGerar_Click(object sender, EventArgs e)
@@ -68,16 +88,11 @@ namespace mockUp
 
             for (int i = 0; i < nudTotalDePacotes.Value; i++)
             {
-                
-                if (i < nudPacoteslFurto.Value) //os primeiros pacotes, vou mandar eles pertecendo a uma mesma regiao geografica inserindo pouca variacao nas coordenadas geograficas
-                    valorASomar = (double)(rnd.Next(-10, 10) / 5000.0); //pequena variacao de posicao
-                else
-                    valorASomar = (double)(rnd.Next(-10, 10) / 50.0); //grande variacao de posicao
-                latitude = latitude + valorASomar;
-                longitude = longitude + valorASomar;
+                if (flagStop)
+                    break;
 
-                //latitude = rnd.NextDouble() * (maxDouble - minDouble) + minDouble;
-                //longitude = rnd.NextDouble() * (maxDouble - minDouble) + minDouble;
+                latitude = rnd.NextDouble() * (maxLat - minLat) + minLat;
+                longitude = rnd.NextDouble() * (maxLong- minLong) + minLong;
 
 
                 formatoPacoteModulo2 = "{\"Lat\":" + latitude.ToString(CultureInfo.InvariantCulture) +
@@ -93,12 +108,52 @@ namespace mockUp
                     } catch (Exception ex) { }
                 }
             }
+            if (usocketConexaoUDPModulo2 != null)
+                usocketConexaoUDPModulo2.Close();
+            flagStop = false;
         }
 
         private void EnvioPacotesFalta()
         {
+            Random rnd = new Random();
+            double valorASomar = 0;
+            double latMid = rnd.NextDouble() * (maxLat - minLat) + minLat;
+            double longMid = rnd.NextDouble() * (maxLong - minLong) + minLong;
 
+            for (int i = 0; i < nudTotalDePacotes.Value; i++)
+            {
+                if (flagStop)
+                    break;
 
+                if (i < nudPacoteslFurto.Value) //os primeiros pacotes, vou mandar eles pertecendo a uma mesma regiao geografica inserindo pouca variacao nas coordenadas geograficas
+                {
+                    valorASomar = (double)(rnd.Next(-10, 10) / 5000.0); //pequena variacao de posicao
+                    latitude = latMid + valorASomar;
+                    longitude = longMid + valorASomar;
+                }
+                else
+                {
+                    latitude = rnd.NextDouble() * (maxLat - minLat) + minLat;
+                    longitude = rnd.NextDouble() * (maxLong - minLong) + minLong;
+                }
+
+                formatoPacoteModulo2 = "{\"Lat\":" + latitude.ToString(CultureInfo.InvariantCulture) +
+                                       ",\"Long\":" + longitude.ToString(CultureInfo.CreateSpecificCulture("en-GB")) +
+                                       ",\"codErro\":95}";
+
+                bytesAEnviarModulo2 = Encoding.ASCII.GetBytes(formatoPacoteModulo2);
+                if (usocketConexaoUDPModulo2 != null)
+                {
+                    try
+                    {
+                        usocketConexaoUDPModulo2.Send(bytesAEnviarModulo2, bytesAEnviarModulo2.Length, ipConexaoEnvioUDPModulo2);
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+            if (usocketConexaoUDPModulo2 != null)
+                usocketConexaoUDPModulo2.Close();
+            flagStop = false;
 
         }
 
@@ -121,10 +176,5 @@ namespace mockUp
             }
         }
 
-        private void btnParar_Click(object sender, EventArgs e)
-        {
-            if (usocketConexaoUDPModulo2 != null)
-                usocketConexaoUDPModulo2.Close();  
-        }
     }
 }
